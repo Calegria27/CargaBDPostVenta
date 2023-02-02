fcd6bc4ddb72d692c6e377f2fcc899a8b38e9a8a
Calegria27 <carlos.alegria@cindependencia.cl>
2023-02-02 10:15:20 -0300

    update

--- BDPostVenta.py
+++ BDPostVenta.py
@@ -23,12 +23,10 @@ except pyodbc.Error as e:
     print(str(e.value[1]))
 
 cursor = conn.cursor()
-cursor.execute("SELECT * FROM dbo.V_INFORME_DETALLE_VIVIENDA_RECLAMOOT_ACTIVA")
+cursor.execute("SELECT     dbo.V_DETALLE_RECLAMOS_OT.id_reclamo, dbo.V_DETALLE_RECLAMOS_OT.id_ot, dbo.RECLAMOS.id_propiedad, dbo.V_DETALLE_RECLAMOS_OT.nombre_obra,                       dbo.V_DETALLE_RECLAMOS_OT.nombre_etapa, dbo.V_DETALLE_RECLAMOS_OT.direccion, dbo.V_DETALLE_RECLAMOS_OT.lote, dbo.V_DETALLE_RECLAMOS_OT.manzana,                       dbo.V_DETALLE_RECLAMOS_OT.nombres, dbo.V_DETALLE_RECLAMOS_OT.estado, dbo.V_DETALLE_RECLAMOS_OT.recinto, dbo.V_DETALLE_RECLAMOS_OT.lugar,                       dbo.V_DETALLE_RECLAMOS_OT.item, dbo.V_DETALLE_RECLAMOS_OT.problema, CAST(dbo.V_DETALLE_RECLAMOS_OT.fecha_reclamo AS DATE)as Fecha_Reclamo, CAST(dbo.V_DETALLE_RECLAMOS_OT.fecha_conformiad as DATE)as Fecha_Conformidad,                       dbo.V_DETALLE_RECLAMOS_OT.conformidad, dbo.MAESTRO_ESTADOS.descripcion, dbo.RECHAZOCALIDAD.DESCRIPCION AS Expr1, dbo.MOTIVOPOSTERGADO.POSTERGADO, dbo.V_DETALLE_RECLAMOS_OT.telefono1 FROM         dbo.V_DETALLE_RECLAMOS_OT INNER JOIN                      dbo.RECLAMOS ON dbo.V_DETALLE_RECLAMOS_OT.id_reclamo = dbo.RECLAMOS.id_reclamo INNER JOIN                      dbo.ORDENES_TRABAJO ON dbo.V_DETALLE_RECLAMOS_OT.id_ot = dbo.ORDENES_TRABAJO.id_ot INNER JOIN                      dbo.MAESTRO_ESTADOS ON dbo.RECLAMOS.estado = dbo.MAESTRO_ESTADOS.codigo_estado INNER JOIN                      dbo.RECHAZOCALIDAD ON dbo.ORDENES_TRABAJO.rechazocalidad = dbo.RECHAZOCALIDAD.id INNER JOIN                      dbo.MOTIVOPOSTERGADO ON dbo.ORDENES_TRABAJO.motivopostergado = dbo.MOTIVOPOSTERGADO.id Where dbo.V_DETALLE_RECLAMOS_OT.fecha_reclamo >= '2021-1-10'")
 data=cursor.fetchall()
 data_PV=pd.DataFrame(np.array(data)) 
-data_PV.pop(data_PV.columns[-1])
-data_PV.pop(data_PV.columns[-1])
-print(data_PV)
+
 """
 Getting  Google Sheets
 """
@@ -39,7 +37,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION,SCOPES )
 
-google_sheets_id = '1QylL7IcQn825ycs_qMcnP30tJuY6YK64_epow64q1eM'
+google_sheets_id = '1ZcOciAFVD66ll0hcACAGaLLueu01ofQGXCLqzLce9RQ'
 
 def construct_request_body(value_array, dimension: str='ROWS') -> dict:
     try:
@@ -55,21 +53,21 @@ def construct_request_body(value_array, dimension: str='ROWS') -> dict:
 response = service.spreadsheets().values().get(
     spreadsheetId=google_sheets_id,
     majorDimension='ROWS',
-    range='BD!A2:M'
+    range='SABANA!A2:V'
     ).execute()
 
-print(data_PV)
+
 recordset = data_PV.values.tolist()
 
 """
 Insert rows
 """
 request_body_values = construct_request_body(recordset)
-service.spreadsheets().values().clear(spreadsheetId=google_sheets_id, range='BD!A2:M').execute()
+service.spreadsheets().values().clear(spreadsheetId=google_sheets_id, range='SABANA!A2:V').execute()
 service.spreadsheets().values().update(
     spreadsheetId=google_sheets_id,
     valueInputOption='USER_ENTERED',
-    range='BD!A2:M',
+    range='SABANA!A2:V',
     body=request_body_values
     ).execute()
 
